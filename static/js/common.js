0d64df3f40b6bacc2bf0344864673cfa8fec6d5e
L-HJ <74453343+L-HJ@users.noreply.github.com>
2023-09-04 18:33:13 +0900

    gabojang
    울산시장앱 가보장

---
+++ static/js/common.js
@@ -0,0 +1,31 @@
+$(function() {
+});
+
+//레이어 팝업 공통
+function commonPop(name, option) {
+    var opt = {
+        escape:false,
+        scrolllock: true,
+        transition: 'all 0.3s',
+        opacity: 0.6
+    }
+    $.extend(opt, option);
+    $(name).popup(opt);
+}
+
+// 22.07.01 이현진 장바구니 토스트 팝업 추가
+function toast(msg, timer) {
+	if (!timer) { timer = 2000; }
+	var $elem = $("<div class='toastWrap'><span class='toast'>" + msg + "</span></div>");
+	$("#toast").append($elem); //top = prepend, bottom = append
+	$elem.slideToggle(100, function() {
+		$('.timerWrap', this).first().outerWidth($elem.find('.toast').first().outerWidth() - 10);
+		if (!isNaN(timer)) {
+			setTimeout(function() {
+				$elem.fadeOut(function() {
+					$(this).remove();
+				});
+			}, timer);			
+		}
+	});
+}
